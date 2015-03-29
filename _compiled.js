5ff5ead65e8e7dfa85cda46314559bfb26796be5
danpaul <pt2323@gmail.com>
2015-03-29 10:01:10 -0400

    update position

--- _compiled.js
+++ _compiled.js
@@ -27,10 +27,14 @@ var geoLit = {}
 
 *******************************************************************************/
 
-geoLit.map
-geoLit.currentLatitude
-geoLit.currentLongitude
-geoLit.mapId
+geoLit.map = null
+geoLit.currentLatitude = null
+geoLit.currentLongitude = null
+geoLit.intervalId = null
+geoLit.intervalTime = 1000 * 10 //10 seconds
+// geoLit.intervalTime = 1000 //10 seconds
+geoLit.mapId = null
+geoLit.userMarker = null
 geoLit.zoomLevel = 12
 
 /*******************************************************************************
@@ -50,6 +54,23 @@ geoLit.createMap = function(){
                                      mapOptions);
 }
 
+geoLit.init = function(mapId, callbackIn){
+
+    geoLit.mapId = mapId
+    google.maps.event.addDomListener(window, 'load', function(){
+        geoLit.updatePosition(function(err){
+            if( err ){ callbackIn(err) }
+            else{
+                geoLit.createMap()
+                geoLit.updateUserMarker()
+                geoLit.intervalId = window.setInterval(geoLit.intervalCallback,
+                                                       geoLit.intervalTime)
+                callbackIn()
+            }
+        })
+    })
+}
+
 geoLit.updatePosition = function(callbackIn){
     navigator.geolocation.getCurrentPosition(function(position){
         geoLit.currentLatitude = position.coords.latitude
@@ -58,16 +79,30 @@ geoLit.updatePosition = function(callbackIn){
     }, callbackIn)
 }
 
-geoLit.init = function(mapId, callbackIn){
-    geoLit.mapId = mapId
-    google.maps.event.addDomListener(window, 'load', function(){
-        geoLit.updatePosition(function(err){
-            if( err ){ callbackIn(err) }
-            else{
-                geoLit.createMap()
-                callbackIn()
-            }
-        })
+geoLit.updateUserMarker = function(){
+
+    // delete existing marker
+    if( geoLit.userMarker !== null ){ geoLit.userMarker.setMap(null) }
+
+    // set new marker
+    var latLang = new google.maps.LatLng(geoLit.currentLatitude,
+                                         geoLit.currentLongitude);
+
+    geoLit.userMarker = new google.maps.Marker({
+      position: latLang,
+      map: geoLit.map,
+      title: 'Current Position'
+    });
+}
+
+geoLit.intervalCallback = function(){
+console.log(geoLit.currentLatitude + ", " + geoLit.currentLongitude)
+    geoLit.updatePosition(function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+        geoLit.updateUserMarker
     })
 }
 
